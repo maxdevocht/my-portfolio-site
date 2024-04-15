34f51be1afd3364496192fa8d3bf0d8cc71d811b
Max de Vocht <maxdevocht@hotmail.com>
2024-04-15 16:21:45 +0200

    Added Header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 import './App.css'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header--info">
+                <h1 className="name">I'm Max</h1>
+                <h2 className="bio">Front-end Dev</h2>
+            </div>
+
+            <div className="header--img"></div>
+        </header>
+    )
+}
