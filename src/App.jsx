f802891a1e46f8c16e4078e53aa07ee54aed4f1e
Max de Vocht <maxdevocht@hotmail.com>
2024-04-17 14:05:16 +0200

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,16 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Links from './components/Links'
-import linkdata from '../linkdata'
 import './App.css'
 
 export default function App() {
-
-  const links = linkdata.map(item => {
-    return (
-      <Links 
-        key={item.id}
-        item={item}
-      />
-    )
-  })
 
   return (
     <>
       <Navbar />
       <Header />
-      <section className="section--links">
-        {links}
-      </section>
+      <Links />
+      <p className="copyright"><i class="ri-copyright-fill"></i> 2024 Max de Vocht. All Rights Reserved</p>
     </>
   )
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
-export default function Links(props) {
+export default function Links() {
     return (
-        <button className="link--btn">
-            <h2 className="link--title">{props.item.title}</h2>
-        </button>
+        <section className="section--links">
+            <div className="first--section">
+                <div className="first--btns">
+                    <button className="link--btn shadow">
+                        <h2 className="link--title">BQ Music</h2>
+                    </button>
+                    <button className="link--btn shadow">
+                        <h2 className="link--title">My Shopping List</h2>
+                    </button>
+                </div>
+                <button className="link--btn shadow">
+                        <h2 className="link--title">Link Catcher Chrome Extension</h2>
+                </button>
+            </div>
+            <div className="second--section">
+                <div className="second--btns">
+                    <button className="link--btn shadow">
+                        <h2 className="link--title">Responsive Cactus Site</h2>
+                    </button>
+                    <button className="link--btn shadow">
+                        <h2 className="link--title">My ToDo App</h2>
+                    </button>
+                </div>
+                <button className="link--btn shadow">
+                        <h2 className="link--title">My Travel Journal</h2>
+                </button>
+            </div>
+        </section>
         
     )
 }
