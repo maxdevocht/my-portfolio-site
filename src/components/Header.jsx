fd6927f1e38da89ac1187fe235bf861da564ce7a
Max de Vocht <maxdevocht@hotmail.com>
2024-04-17 11:29:43 +0200

    Added img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header">
-            <div className="header--info">
+            <div className="header--info shadow">
                 <h1 className="name">I'm Max 👋</h1>
                 <h2 className="bio">Front-end Dev</h2>
             </div>
 
-            <div className="header--img"></div>
+            <div className="header--img shadow"></div>
         </header>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
+        <nav className="navbar shadow">
             <ul className="nav--list">
                 <li className="list--item"><a href="#"><i class="ri-instagram-fill"></i></a></li>
                 <li className="list--item"><a href="#"><i class="ri-github-fill"></i></a></li>
